0af44823347acc8cea36475f0220a24dea208ee1
Ronny Krone <r.krone.rk@gmail.com>
2023-09-03 14:12:23 +0200

    prepare publishing

--- test/simple_dart_logger_test.dart
+++ test/simple_dart_logger_test.dart
@@ -1,16 +1,6 @@
-import 'package:simple_dart_logger/simple_dart_logger.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
   });
 }
